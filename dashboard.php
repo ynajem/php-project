5db7dc0f85b78212f364da294e36338839c824ff
= <=>
2018-11-26 18:18:09 +0100

    Add git ignore

--- dashboard.php
+++ dashboard.php
@@ -1,17 +1,19 @@
 <?php
   $page_title = "Najem Dashboard";
+  $page_description = "Description Here"; 
 ?>
 <!doctype html>
 <html lang="en">
 <head>
   <?php include("inc/inc.head.php") ?>
+  <link rel="stylesheet" href="css/main.css"> 
 </head>
 
 <body class="app">
   <?php include "inc/inc.dashboard.header.php" ?>
   <div id="main-content">
     <?php include "inc/inc.sidebar.php" ?>
-    <main id="content" class="p-2">
+    <main id="content" class="p-2"> 
       <div class="card">Cool Down</div>
     </main>
   </div>
